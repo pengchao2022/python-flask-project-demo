5ae5ec0a2cd5113b4be81dc5696ab3a87a97f31d
pengchao <pengchao.ma2@outlook.com>
2025-09-04 09:20:14 +0800

    updated

--- terraform-infra-ec2/main.tf
+++ terraform-infra-ec2/main.tf
@@ -3,11 +3,11 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.0"
+      version = "~> 5.0"
     }
   }
 }
-
+ 
 provider "aws" {
   region = var.aws_region
 }
